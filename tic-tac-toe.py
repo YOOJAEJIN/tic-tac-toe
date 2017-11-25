b047c6e854ce4229edef2078b251e5783b71c1ab
YooJaejin <jjy1715@gmail.com>
2017-11-24 18:06:15 -0800

    fisrt

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,34 @@
 from tkinter import *
+import tkinter.messagebox
+
+
+def win():
+      if player == "O":
+            tkinter.messagebox.showinfo("","Green win!")
+            quit()
+      else:
+            tkinter.messagebox.showinfo("","Yello win!")
+            quit()
+
+
+def check():
+      if list[0]["text"] == list[3]["text"] == list[6]["text"] != "     ":
+            win()
+      elif list[1]["text"] == list[4]["text"] == list[7]["text"] != "     ":
+            win()
+      elif list[2]["text"] == list[5]["text"] == list[8]["text"] != "     ":
+            win()
+      elif list[0]["text"] == list[1]["text"] == list[2]["text"] != "     ":
+            win()
+      elif list[3]["text"] == list[4]["text"] == list[5]["text"] != "     ":
+            win()
+      elif list[6]["text"] == list[7]["text"] == list[8]["text"] != "     ":
+            win()
+      elif list[0]["text"] == list[4]["text"] == list[8]["text"] != "     ":
+            win()
+      elif list[2]["text"] == list[4]["text"] == list[6]["text"] != "     ":
+            win()
+      
 
 def checked(i) :
       global player
@@ -10,9 +40,11 @@ def checked(i) :
       button["bg"] = "yellow"
 
       if player == "X" :
+            check()
             player = "O"
             button["bg"] = "yellow"
       else :
+            check()
             player = "X"
             button["bg"] = "lightgreen"
 
